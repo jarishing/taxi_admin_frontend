86d9a3227b257ab24e1ab8820036f548c5238d92
Yuen Ka Shing <jarishing@gmail.com>
2019-02-25 16:50:32 +0800

    change url

--- src/constant/base.js
+++ src/constant/base.js
@@ -1,6 +1,6 @@
 // export const baseUrl = 'https://taxi.ngrok.kenyip.cc/api';
 // export const imageUrl = 'https://taxi.ngrok.kenyip.cc';
-export const baseUrl = 'http://localhost:3100/api';
-export const imageUrl = 'http://localhost:3100';
-// export const baseUrl = 'http://admin.hkairportqualitytaxi.com/api';
-// export const imageUrl = 'http://admin.hkairportqualitytaxi.com';
+// export const baseUrl = 'http://localhost:3100/api';
+// export const imageUrl = 'http://localhost:3100';
+export const baseUrl = 'http://admin.hkairportqualitytaxi.com/api';
+export const imageUrl = 'http://admin.hkairportqualitytaxi.com';
